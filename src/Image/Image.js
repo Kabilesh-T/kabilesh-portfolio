4aa2bb7e271c8c6d13009bcd28fa22099eda2f43
Kabilesh T <iamkabi@gmail.com>
2021-07-07 12:26:41 +0530

    new look

--- src/Image/Image.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Image.scss"
-
-const Image = ({ number }) => {
-    const classNameOption = `Image${number}`
-    return <div className={classNameOption}/>
-};
-export default Image;
